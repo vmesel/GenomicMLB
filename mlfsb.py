34b6263d9a27e6a3866c24d48ef1f33b35e20aca
Vinicius Mesel <vinicius@bigsp.com.br>
2016-09-02 01:06:00 -0300

    New Creator

--- mlfsb.py
+++ mlfsb.py
@@ -3,55 +3,56 @@ import os
 import random
 
 def baserandomica():
-	bases = ["A", "C", "T", "G"]
-	return random.choice(bases)
-	
+    bases = ["A", "C", "T", "G"]
+    return random.choice(bases)
+    
 def gerasequencia(length):
-	sequencia = []
-	length = int(length) - 1
-	for i in range(length):
-		sequencia.append(baserandomica())
-	return "".join(sequencia)
+    sequencia = []
+    length = int(length) - 1
+    for i in range(length):
+        sequencia.append(baserandomica())
+    return "".join(sequencia)
 
 def gerasequenciacompadrao(length, padrao):
-	_pdr_tam = len(padrao)
-	_len_seq_sp = int(length) - int(_pdr_tam)
-	_separador = random.randint(0, _len_seq_sp)
-	
-	_seqa = gerasequencia(_separador)
-	_seqb_valor = length - _pdr_tam - _separador
-	_seqb = gerasequencia(_seqb_valor)
-	
-	return _seqa + padrao + _seqb
-	
+    _pdr_tam = len(padrao)
+    _len_seq_sp = int(length) - int(_pdr_tam)
+    _separador = random.randint(0, _len_seq_sp)
+    
+    _seqa = gerasequencia(_separador)
+    _seqb_valor = length - _pdr_tam - _separador
+    _seqb = gerasequencia(_seqb_valor)
+    
+    return _seqa + padrao + _seqb
+    
 def geratuplas(sequencias, length, padrao):
-	#TODO: Implement a pattern list reader, for generating more patterns at once
-	_seqlist = []
-	for i in range(sequencias - 1):
-		_seqlist.append(random.randint(0,1))
-	
-	outputs = []
-	for i in _seqlist:
-		if i == 0:
-			outputs.append(gerasequencia(length))
-		else:
-			outputs.append(gerasequenciacompadrao(length, padrao))
+    #TODO: Implement a pattern list reader, for generating more patterns at once
+    _seqlist = []
+    for i in range(sequencias - 1):
+        _seqlist.append(random.randint(0,1))
+    
+    outputs = []
+    for i in _seqlist:
+        if i == 0:
+            outputs.append(gerasequencia(length))
+        else:
+            outputs.append(gerasequenciacompadrao(length, padrao))
 
-	return zip(_seqlist, outputs)
+    return zip(_seqlist, outputs)
 
-def gerafasta(sequencias, length, padrao, fasta):
-	_tuples = geratuplas(sequencias, length, padrao)
+def gerafasta(sequencias, length, padrao, fasta1, fasta2):
+    _tuples = geratuplas(sequencias, length, padrao)
 
-	f = open(fasta, "r+")	
+    f = open(fasta1, "w")
+    z = open(fasta2, "w")
 
-	for item in _tuples:
-		tipo, valor = item
-		if tipo == 0:
-			f.write(">seq_sem_padrao:RANDOM\n")
-			f.write(valor + "\n")
-		else:
-			f.write(">seq_com_padrao:RANDOMCPADRAO\n")
-			f.write(valor + "\n")
-	return True
+    for item in _tuples:
+        tipo, valor = item
+        if tipo == 0:
+            f.write(">seq_sem_padrao:RANDOM\n")
+            f.write(valor + "\n")
+        else:
+            z.write(">seq_com_padrao:RANDOMCPADRAO\n")
+            z.write(valor + "\n")
+    return True
 
-print(gerafasta(10, 32, "PADRAOAQUI", "fasta-out.fasta"))
+print(gerafasta(1000, 100, "AACCTTGG", "seq_0.fasta", "seq_1.fasta"))
